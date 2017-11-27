3d42b51115d723390d1ca7cf76d2307844057947
RAJ123SONY <rakeshsony18@gmail.com>
2017-11-28 00:56:10 +0530

    Always To Use Annotations
    FosBundle Changes RakeshRaj

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -4,15 +4,17 @@ namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/*
+ * The action's view can be rendered using render() method
+ * @Template()
+ * @Route()
+ *
+ */
 class WelcomeController extends Controller
 {
     public function indexAction()
     {
-        /*
-         * The action's view can be rendered using render() method
-         * or @Template annotation as demonstrated in DemoController.
-         *
-         */
+        
         return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
     }
 }
